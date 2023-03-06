1bccdfa8569c90e5052913c715a8b8d0e1a6ce83
MacNikSipeykin <niksipeykin@mail.ru>
2023-03-06 22:21:48 +0300

    create uniq keys

--- src/App.js
+++ src/App.js
@@ -1,7 +1,51 @@
+import { NewsItem } from "./NewsItem/NewsItem";
+
+const news =[
+  {
+    
+      title: 'First news',
+      url: 'example.com',
+      userName: 'User 1',
+      date: '10.10.11',
+      score: 120,
+      id: 1
+    },
+   {
+      title: 'Second news',
+      url: 'example.com',
+      userName: 'User 2',
+      date: '10.03.11',
+      score: 3,
+      id: 2
+    },
+     {
+      title: 'Third news',
+      url: 'example.com',
+      userName: 'User 3',
+      date: '10.20.11',
+      score: 1022,
+      id: 3
+    }
+    
+  
+]
+
+
 function App() {
   return (
-    <div >
-    </div>
+    <>
+      {
+        news.map(item => {
+          return <NewsItem 
+            key= {item.id}
+            title= {item.title} 
+            url= {item.url} 
+            userName= {item.userName} 
+            date={item.date} 
+            score= {item.score}/>
+        })
+      }
+    </>
   );
 }
 
